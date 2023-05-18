90a56872d143ee6198aeb7d326b2d7bbfcb621cb
alex <alexvetrici@gmail.com>
2023-05-18 16:12:05 +0200

    add stack in python

---
+++ stack/stack.py
@@ -0,0 +1,18 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, data):
+        self.stack.append(data)
+
+    def pop(self):
+        self.stack.pop()
+
+    def isEmpty(self):
+        return len(self.stack) == 0
+
+    def show(self):
+        return self.stack
+
+    def size(self):
+        return len(self.stack)
