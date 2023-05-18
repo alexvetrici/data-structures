9635ff68bcda96e675265f4bd6e4a5310b27fa1e
alex <alexvetrici@gmail.com>
2023-05-18 16:11:29 +0200

    add queue in python

---
+++ queue/queue.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+class Queue:
+    def __init__(self):
+        self.queue = deque()
+    
+    def enqueue(self, data):
+        self.queue.append(data)
+
+    def dequeue(self):
+        self.queue.popleft()
+
+    def isEmpty(self):
+        return len(self.queue) == 0
+
+    def show(self):
+        return self.queue
+
+    def size(self):
+        return len(self.queue)
+
+    def peek(self):
+        return self.queue[0]
