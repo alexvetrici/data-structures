ee152ef3adce43960bd403752e12daf8373a574c
alex <alexvetrici@gmail.com>
2023-06-15 09:07:04 +0200

    BigTree package added

--- tree/python/tree.py
+++ tree/python/tree.py
@@ -32,4 +32,32 @@ root.left.right = Node(9)
 root.right.left = Node(1)
 root.right.right = Node(3)
 
-postorder(root)
+
+# Tree Implementation with BigTree package
+
+# Run 'pip install bigtree' to install the package
+
+from bigtree import Node, preorder_iter, postorder_iter, inorder_iter
+
+root = Node(10)
+b = Node(78, parent=root)
+c = Node(2, parent=root)
+d = Node(54, parent=b)
+e = Node(9, parent=b)
+f = Node(1, parent=c)
+g = Node(3, parent=c)
+
+root.show()
+# 10
+# ├── 78
+# │   ├── 54
+# │   └── 9
+# └── 2
+#     ├── 1
+#     └── 3
+
+preorder = [node.name for node in preorder_iter(root)]
+postorder = [node.name for node in postorder_iter(root)]
+
+print(preorder) # [10, 78, 54, 9, 2, 1, 3]
+print(postorder) # [54, 9, 78, 1, 3, 2, 10]
