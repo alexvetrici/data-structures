9321f45ad8964c8f05f413214e237131192a3119
alex <alexvetrici@gmail.com>
2023-06-16 12:51:28 +0200

    create tree.py and implement bigtree

--- tree/python/tree.py
+++ tree/python/tree.py
@@ -1,95 +1,3 @@
-# Tree implementation 
-
-class Node:
-    # Tree Node structure
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
-
-def depth(root):
-    current_depth = 0
-
-    if root.left:
-        current_depth = max(current_depth, depth(root.left))
-
-    if root.right:
-        current_depth = max(current_depth, depth(root.right))
-
-    return current_depth + 1
-
-
-# Inorder Traversal : left --> root --> right
-def inorder(node):
-    if node:
-        inorder(node.left)
-        print(node.data)
-        inorder(node.right)
-
-# Preorder Traversal : root --> left --> right
-def preorder(node):
-    if node:
-        print(node.data)
-        preorder(node.left)
-        preorder(node.right)
-
-# Postorder Traversal : left --> right --> root
-def postorder(node):
-    if node:
-        postorder(node.left)
-        postorder(node.right)
-        print(node.data)
-
-# Checking if is Full Binary Tree
-def isFullTree(root):
-
-    # An empty tree is Full
-    if root is None:
-        return True
-    
-    if root.left is None and root.right is None:
-        return True
-    
-    if root.left is not None and root.right is not None:
-        return (isFullTree(root.left) and isFullTree(root.right))
-    
-    return False
-
-# Checking if is Perfect Binary Tree
-def isPerfectTree(root, depth, level=0):
-
-    # An empty tree is Perfect
-    if root is None:
-        return True
-    
-    if root.left is None and root.right is None:
-        return(depth == level + 1)
-    
-    if root.left is None or root.right is None:
-        return False
-    
-    return (isPerfectTree(root.left, depth, level + 1) and 
-                isPerfectTree(root.right, depth, level + 1))
-
-root = Node(10)
-root.left = Node(78)
-root.right = Node(2)
-root.left.left = Node(54)
-root.left.right = Node(9)
-root.right.left = Node(1)
-root.right.right = Node(3)
-
-if isFullTree(root):
-    print('This is a Full Binary Tree')
-else:
-    print('This is not a Binary Full Tree')
-
-if isPerfectTree(root, depth(root)):
-    print('This is a Perfect Binary Tree')
-else:
-    print('This is not a Perfect Binary Tree')
-
-
 # Tree Implementation with BigTree package
 # Run 'pip install bigtree' to install the package
 
@@ -117,3 +25,24 @@ postorder = [node.name for node in postorder_iter(root)]
 
 print(preorder) # [10, 78, 54, 9, 2, 1, 3]
 print(postorder) # [54, 9, 78, 1, 3, 2, 10]
+
+
+#Tree implementation from scratch
+
+# Tree implementation 
+
+class Node:
+    # Tree Node structure
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+# Tree construction
+root = Node(10)                         # 10
+root.left = Node(78)                    # ├── 78
+root.right = Node(2)                    # │   ├── 54
+root.left.left = Node(54)               # │   └── 9
+root.left.right = Node(9)               # └── 2
+root.right.left = Node(1)               #     ├── 1
+root.right.right = Node(3)              #     └── 3
