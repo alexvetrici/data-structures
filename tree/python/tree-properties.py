6b37007f9675086af4346cc43e56f1f3358274d9
alex <alexvetrici@gmail.com>
2023-06-16 13:18:49 +0200

    check if a tree is complete

--- tree/python/tree-properties.py
+++ tree/python/tree-properties.py
@@ -1,4 +1,4 @@
-# Tree implementation 
+# Tree properties implementation
 
 class Node:
     # Tree Node structure
@@ -17,6 +17,11 @@ def depth(root):
         current_depth = max(current_depth, depth(root.right))
 
     return current_depth + 1
+
+def node_count(root):
+    if root is None:
+        return 0
+    return 1 + node_count(root.left) + node_count(root.right)
 
 
 # Inorder Traversal : left --> root --> right
@@ -68,8 +73,33 @@ def isPerfectTree(root, depth, level=0):
     if root.left is None or root.right is None:
         return False
     
-    return (isPerfectTree(root.left, depth, level + 1) and 
-                isPerfectTree(root.right, depth, level + 1))
+    return (isPerfectTree(root.left, depth, level + 1) 
+                and isPerfectTree(root.right, depth, level + 1))
+
+# Check if is Complete Binary Tree
+def isCompleteTree(root):
+    if root is None:
+        return True
+    
+    queue = [root]
+    found_null = False
+
+    while queue:
+        node = queue.pop(0)
+        
+        # Check if a non-null node has been found after a null node
+        if node is None:
+            found_null = True
+        else:
+            # If a null node has been found previously, tree is not complete
+            if found_null:
+                return False
+            
+            queue.append(node.left)
+            queue.append(node.right)
+    
+    return True
+
 
 
 # Tree construction
@@ -81,14 +111,23 @@ root.left.right = Node(9)               # └── 2
 root.right.left = Node(1)               #     ├── 1
 root.right.right = Node(3)              #     └── 3
 
-# Check tree properties
+rootIndex = 0
+
+# Check Tree properties
+
 if isFullTree(root):
     print('This is a Full Binary Tree')
 else:
     print('This is not a Binary Full Tree')
+
 
 if isPerfectTree(root, depth(root)):
     print('This is a Perfect Binary Tree')
 else:
     print('This is not a Perfect Binary Tree')
 
+
+if isCompleteTree(root):
+    print("This is a Complete Binary Tree")
+else:
+    print("This is not a Complete Binary Tree")
